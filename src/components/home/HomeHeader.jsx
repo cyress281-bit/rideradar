aa47218b9a8d9ea28d9516182d046951e703d810
cyress281 <cyress281@icloud.com>
2026-03-22 20:26:37 +0000

    File changes

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -23,19 +23,11 @@ export default function HomeHeader({ username }) {
           animate={{ scale: 1, opacity: 1 }}
           className="relative"
         >
-          {settings.logo_url ? (
-            <img
-              src={settings.logo_url}
-              alt={settings.name}
-              className="w-9 h-9 rounded-xl object-cover shadow-[0_0_14px_rgba(0,240,50,0.6)]"
-            />
-          ) : (
-            <div className="w-9 h-9 rounded-xl bg-primary flex items-center justify-center shadow-[0_0_14px_rgba(0,240,50,0.6)]">
-              <svg viewBox="0 0 24 24" className="w-5 h-5 text-primary-foreground fill-current">
-                <path d="M3 12c0-1.5 1.5-3 3-3s3 1.5 3 3-1.5 3-3 3-3-1.5-3-3zm14 0c0-1.5 1.5-3 3-3s3 1.5 3 3-1.5 3-3 3-3-1.5-3-3zm-7-8h2v6h-2zM7 8l-3 3 1.4 1.4L7 10.8l3.6 3.6L12 13l-5-5zm10 0l-5 5 1.4 1.4L17 10.8l3.6 3.6L22 13l-5-5z" />
-              </svg>
-            </div>
-          )}
+          <img
+            src="https://media.base44.com/images/public/69c00371996a3698d9301734/ae383836a_image.png"
+            alt={settings.name}
+            className="w-9 h-9 rounded-xl object-cover shadow-[0_0_14px_rgba(0,240,50,0.6)]"
+          />
           <div className="absolute -top-0.5 -right-0.5 w-2.5 h-2.5 bg-primary rounded-full border-2 border-background shadow-[0_0_6px_rgba(0,240,50,0.8)]" />
         </motion.div>
         <div>
